d9ce552e65c9c31a312be844bd7a9c8702c1596d
Andrew Ross <andrross@amazon.com>
2022-05-10 00:17:47 +0000

    Updates for changes in hppc classes
    There are 3 changes in hppc that impact OpenSearch:
    
    - ObjectIntScatterMap has been removed. I could have replaced it with
    ObjectIntHashMap, but a plain java.util.HashMap seems to solve the
    problem well so I used that instead.
    
    - `indexRemove(int index)` is a new method in two interfaces that we
    extend in a delegate pattern, so I have had to add that method as a
    pass-through in two places.
    
    - The seeded version of `BitMixer.mix(int, int)` [has been removed][1].
    The implementation that was removed simply xor'd the value to the seed
    before calling the single-int variant of the method. I have changed the
    OpenSearch code to do the xor directly, so this should be equivalent to
    the previous implementation.
    
    [1]: https://github.com/carrotsearch/hppc/pull/10
    
    Signed-off-by: Andrew Ross <andrross@amazon.com>

--- server/src/main/java/org/opensearch/common/lucene/search/function/RandomScoreFunction.java
+++ server/src/main/java/org/opensearch/common/lucene/search/function/RandomScoreFunction.java
@@ -61,7 +61,7 @@ public class RandomScoreFunction extends ScoreFunction {
     public RandomScoreFunction(int seed, int salt, IndexFieldData<?> uidFieldData) {
         super(CombineFunction.MULTIPLY);
         this.originalSeed = seed;
-        this.saltedSeed = BitMixer.mix(seed, salt);
+        this.saltedSeed = BitMixer.mix(seed ^ salt);
         this.fieldData = uidFieldData;
     }
 
@@ -82,7 +82,7 @@ public class RandomScoreFunction extends ScoreFunction {
             public double score(int docId, float subQueryScore) throws IOException {
                 int hash;
                 if (values == null) {
-                    hash = BitMixer.mix(ctx.docBase + docId, saltedSeed);
+                    hash = BitMixer.mix((ctx.docBase + docId) ^ saltedSeed);
                 } else if (values.advanceExact(docId)) {
                     hash = StringHelper.murmurhash3_x86_32(values.nextValue(), saltedSeed);
                 } else {
